bc0f6ccc62c743443c448fef3f0ffedbdd831bb2
[KORBEOM] <[rkdtmdqja98@naver.com]>
2023-06-20 10:33:57 +0900

    루트 추가, 레이아웃 추가

---
+++ lib/layout/toast_message.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+
+
+void toastmsg(String text) {
+  Fluttertoast.showToast(
+      msg: text,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.grey[700],
+      fontSize: 20.0,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT);
+}
