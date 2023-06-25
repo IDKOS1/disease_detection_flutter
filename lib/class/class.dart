47ac659d1e4466c93130317d08c5893f6d875b74
IDKOS1 <kss3736@naver.com>
2023-06-25 20:26:27 +0900

    Images 클래스 추가로 Flie? 변수들 리스트화, 반복문 사용 코드 간결화

---
+++ lib/class/class.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+class Images{
+  late final String name;
+  late final String text;
+  late File? imgPath;
+
+  Images(this.name, this.text, this.imgPath);
+}
--- lib/screen/camera_page.dart
+++ lib/screen/camera_page.dart
@@ -1,9 +1,11 @@
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:untitled/class/class.dart';
 import '../layout/toast_message.dart';
 
 class CameraPage extends StatefulWidget {
@@ -14,26 +16,52 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
-    File? _image,
-        _current,
-        _head,
-        _left,
-        _right,
-        _four,
-        _five,
-        _six,
-        _seven,
-        _eight,
-        _nine,
-        _ten,
-        _eleven,
-        _twelve,
-        _thirteen,
-        _fourteen,
-        _fifteen,
-        _sixteen,
-        _seventeen;
+  File? _current, _image;
 
+
+  static List<String> imgText = [
+    '_head',
+    '_left',
+    '_right',
+    '_four',
+    ' _five',
+    '_six',
+    '_seven',
+    '_eight',
+    '_nine',
+    '_ten',
+    '_eleven',
+    '_twelve',
+    '_thirteen',
+    '_fourteen',
+    '_fifteen',
+    '_sixteen',
+    '_seventeen'
+  ];
+
+  static List<String> imgName = [
+    '머리',
+    '왼쪽 아가미',
+    '오른쪽 아가미',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+  ];
+
+
+  final List<Images> imageData = List.generate(17, (index) =>
+      Images(imgName[index], imgText[index], null));
 
   String _text = '사진을 선택하시오';
   final picker = ImagePicker();
@@ -48,58 +76,10 @@ class _CameraPageState extends State<CameraPage> {
       setState(() {
         _image = File(image!.path);
         _current = _image;
-        switch (_text) {
-          case '머리':
-            _head = _image;
-            break;
-          case '왼쪽 아가미':
-            _left = _image;
-            break;
-          case '오른쪽 아가미':
-            _right = _image;
-            break;
-          case '4':
-            _four = _image;
-            break;
-          case '5':
-            _five = _image;
-            break;
-          case '6':
-            _six = _image;
-            break;
-          case '7':
-            _seven = _image;
-            break;
-          case '8':
-            _eight = _image;
-            break;
-          case '9':
-            _nine = _image;
-            break;
-          case '10':
-            _ten = _image;
-            break;
-          case '11':
-            _eleven = _image;
-            break;
-          case '12':
-            _twelve = _image;
-            break;
-          case '13':
-            _thirteen = _image;
-            break;
-          case '14':
-            _fourteen = _image;
-            break;
-          case '15':
-            _fifteen = _image;
-            break;
-          case '16':
-            _sixteen = _image;
-            break;
-          case '17':
-            _seventeen = _image;
-            break;
+        for(i=0; i<=16; i++){
+          if(imageData[i].name == _text) {
+            imageData[i].imgPath = File(image!.path);
+          }
         }
       });
     }
@@ -112,8 +92,14 @@ class _CameraPageState extends State<CameraPage> {
         Text(_text),
         Container(
             color: Colors.grey,
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
+            height: MediaQuery
+                .of(context)
+                .size
+                .width,
             child: Center(
                 child: _current == null
                     ? const Text('No image')
@@ -122,32 +108,38 @@ class _CameraPageState extends State<CameraPage> {
     );
   }
 
-  Widget grid(String part, File? index) {
+  Widget grid(Images images) {
     return Column(
       children: [
         InkWell(
           onTap: () {
             setState(() {
-              _text = part;
-              _current = index;
+              _text = images.name;
+              _current = images.imgPath;
             });
           },
           child: Container(
             color: Colors.grey,
-            width: MediaQuery.of(context).size.width/4,
-            height: MediaQuery.of(context).size.width/4,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width / 4,
+            height: MediaQuery
+                .of(context)
+                .size
+                .width / 4,
             child: Center(
-                child: index == null
+                child: images.imgPath == null
                     ? const Text('No image')
-                    : Image.file(File(index!.path))),
+                    : Image.file(File(images.imgPath!.path))),
           ),
         ),
-        Text(part)
+        Text(images.name)
       ],
     );
   }
 
-  Widget upgrid(String part, File? index) {
+  Widget upgrid(Images images) {
     return Expanded(
       child: Column(
         children: [
@@ -163,12 +155,18 @@ class _CameraPageState extends State<CameraPage> {
                       },
                       child: Container(
                         color: Colors.white,
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.width,
+                        width: MediaQuery
+                            .of(context)
+                            .size
+                            .width,
+                        height: MediaQuery
+                            .of(context)
+                            .size
+                            .width,
                         child: Center(
-                          child: index == null
+                          child: images.imgPath == null
                               ? const Text('No image')
-                              : Image.file(File(index.path)),
+                              : Image.file(File(images.imgPath!.path)),
                         ),
                       ),
                     ),
@@ -180,22 +178,29 @@ class _CameraPageState extends State<CameraPage> {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 color: Colors.grey,
-                width: MediaQuery.of(context).size.width/3.3,
-                height: MediaQuery.of(context).size.width/3.3,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width / 3.3,
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .width / 3.3,
                 child: Center(
-                    child: index == null
+                    child: images.imgPath == null
                         ? const Text('No image')
-                        : Image.file(File(index!.path))),
+                        : Image.file(File(images.imgPath!.path))),
               ),
             ),
           ),
-          Text(part)
+          Text(images.name)
         ],
       ),
     );
   }
 
   @override
+  int i = 0;
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
@@ -215,40 +220,10 @@ class _CameraPageState extends State<CameraPage> {
                   scrollDirection: Axis.horizontal,
                   child: Row(
                     children: [
-                      grid('머리', _head),
-                      const SizedBox(width: 5),
-                      grid('왼쪽 아가미', _left),
-                      const SizedBox(width: 5),
-                      grid('오른쪽 아가미', _right),
-                      const SizedBox(width: 5),
-                      grid('4', _four),
-                      const SizedBox(width: 5),
-                      grid('5', _five),
-                      const SizedBox(width: 5),
-                      grid('6', _six),
-                      const SizedBox(width: 5),
-                      grid('7', _seven),
-                      const SizedBox(width: 5),
-                      grid('8', _eight),
-                      const SizedBox(width: 5),
-                      grid('9', _nine),
-                      const SizedBox(width: 5),
-                      grid('10', _ten),
-                      const SizedBox(width: 5),
-                      grid('11', _eleven),
-                      const SizedBox(width: 5),
-                      grid('12', _twelve),
-                      const SizedBox(width: 5),
-                      grid('13', _thirteen),
-                      const SizedBox(width: 5),
-                      grid('14', _fourteen),
-                      const SizedBox(width: 5),
-                      grid('15', _fifteen),
-                      const SizedBox(width: 5),
-                      grid('16', _sixteen),
-                      const SizedBox(width: 5),
-                      grid('17', _seventeen),
-                      const SizedBox(width: 5),
+                      for(i = 0; i <= 16; i++) ... [
+                        grid(imageData[i]),
+                        const SizedBox(width:5),
+                      ]
                     ],
                   )),
               Row(
@@ -298,77 +273,23 @@ class _CameraPageState extends State<CameraPage> {
                                 child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
+                                    for(int i=0; i <= 14; i+=2) ... [
+                                      Row(
+                                          mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            upgrid(imageData[i]),
+                                            upgrid(imageData[i+1]),
+                                          ],
+                                      ),
+                                    ],
                                     Row(
                                       mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
+                                      MainAxisAlignment.spaceBetween,
                                       children: [
-                                        upgrid('머리', _head),
-                                        upgrid('왼쪽 아가미', _left)
+                                        upgrid(imageData[16]),
                                       ],
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        upgrid('오른 아가미', _right),
-                                        upgrid('4', _four)
-                                      ],
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        upgrid('5', _five),
-                                        upgrid('6', _six)
-                                      ],
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        upgrid('7', _seven),
-                                        upgrid('8', _eight)
-                                      ],
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        upgrid('9', _nine),
-                                        upgrid('10', _ten)
-                                      ],
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        upgrid('11', _eleven),
-                                        upgrid('12', _twelve),
-                                      ],
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        upgrid('13', _thirteen),
-                                        upgrid('14', _fourteen),
-                                      ],
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        upgrid('15', _fifteen),
-                                        upgrid('16', _sixteen),
-                                      ],
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        upgrid('11', _seventeen),
-                                      ],
-                                    ),
+                                    )
                                   ],
                                 ),
                               ),
